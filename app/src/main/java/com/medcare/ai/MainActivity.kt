0c59241b47a09c86424f2633bf831c54e8cf0d95
abhijeetpatil-hub <patilabhijeet222@gmail.com>
2025-12-31 15:11:59 +0530

    Save local changes before rebase

--- app/src/main/java/com/medcare/ai/MainActivity.kt
+++ app/src/main/java/com/medcare/ai/MainActivity.kt
@@ -1,47 +1,22 @@
 package com.medcare.ai
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.medcare.ai.ui.theme.MedCareClinicalAISuiteTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MedCareClinicalAISuiteTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MedCareClinicalAISuiteTheme {
-        Greeting("Android")
+        // IMPORTANT: local backend until deployment
+        myWebView.loadUrl("http://192.168.1.34:8501/")
+
+
+        setContentView(webView)
     }
 }
